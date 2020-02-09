20b06eca6f13aad29142d4e0f87c52e747aef5c9
rmrotek <remek87@gmail.com>
2020-02-09 21:53:27 +0100

    184

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,42 +1,49 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(key => {
-    return <li key={key}>
-      <span
-        style={{ textTransform: 'capitalize' }}
-      >
-        {key}
-      </span>: {props.ingredients[key]}
-    </li>
-  })
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('order summary will update')
+  }
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(key => {
+      return <li key={key}>
+        <span
+          style={{ textTransform: 'capitalize' }}
+        >
+          {key}
+        </span>: {this.props.ingredients[key]}
+      </li>
+    })
 
-  return (
-    <Fragment>
-      <h3>Your order</h3>
-      <p>Burger with the following ingredients:</p>
+    return (
+      <Fragment>
+        <h3>Your order</h3>
+        <p>Burger with the following ingredients:</p>
 
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType='Danger'
-        clicked={props.purchaseCancelled}
-      >
-        CANCEL
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button
+          btnType='Danger'
+          clicked={this.props.purchaseCancelled}
+        >
+          CANCEL
       </Button>
-      <Button
-        btnType='Success'
-        clicked={props.purchaseContinued}
-      >
-        CONTINUE
+        <Button
+          btnType='Success'
+          clicked={this.props.purchaseContinued}
+        >
+          CONTINUE
       </Button>
-    </Fragment>
-  )
+      </Fragment>
+    )
+  }
+
 }
 
 export default OrderSummary
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import classes from './Layout.module.css'
 
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => (
-  <Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Fragment>
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
 
-)
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => ({showSideDrawer: !prevState.showSideDrawer}))
+  }
+  
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    )
+  }
+}
 
 export default Layout
