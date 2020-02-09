004ace604ffd96193287a0eb96fcf6872f39e51d
rmrotek <remek87@gmail.com>
2020-02-09 21:22:07 +0100

    179

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react'
+
+import Button from '../../UI/Button/Button'
+
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(key => {
+    return <li key={key}>
+      <span
+        style={{ textTransform: 'capitalize' }}
+      >
+        {key}
+      </span>: {props.ingredients[key]}
+    </li>
+  })
+
+  return (
+    <Fragment>
+      <h3>Your order</h3>
+      <p>Burger with the following ingredients:</p>
+
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button
+        btnType='Danger'
+        clicked={props.purchaseCancelled}
+      >
+        CANCEL
+      </Button>
+      <Button
+        btnType='Success'
+        clicked={props.purchaseContinued}
+      >
+        CONTINUE
+      </Button>
+    </Fragment>
+  )
+}
+
+export default OrderSummary
