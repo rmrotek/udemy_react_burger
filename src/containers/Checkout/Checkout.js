6fc2ead1f55f0d5cd2e11f76e421da2d790de893
Remigiusz Mrotek <r.mrotek@XTRACK.local>
2020-06-01 10:56:57 +0200

    315 - fetching orders

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -18,15 +18,20 @@ class Checkout extends Component {
 
   render() {
     let summary = <Redirect to='/' />
+
     if (this.props.ings) {
-      summary = <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-        />
-        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
-      </div>
+      const purchasedRedirect = this.props.purchased ? <Redirect to='/' /> : null
+      summary = (
+        <div>
+          {purchasedRedirect}
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+        </div>
+      )
     }
     return (
       summary
@@ -36,8 +41,11 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   }
 }
+
+
 
 export default connect(mapStateToProps)(Checkout)
