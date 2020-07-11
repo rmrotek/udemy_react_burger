1765fe8edb363ceddc010608dd63d90659bcd8a0
rmrotek <remek87@gmail.com>
2020-07-12 00:08:38 +0200

    344

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
           <Route path='/logout' exact component={Logout} />
+          <Route path='/auth' component={Auth} />
           <Route path='/' exact component={BurgerBuilder} />
         </Switch>
       )
